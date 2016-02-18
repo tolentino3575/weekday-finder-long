02afbf4410c3934a6113ef08be98064ec4dbe165
Adam Ross Russell and erik tolentino <tolentino3575@gmail.com>
2016-02-18 14:30:42 -0800

    Refactor

--- tests/LeapYearTest.php
+++ tests/LeapYearTest.php
@@ -17,31 +17,44 @@
 		// //Assert
 		// $this->assertEquals('14', $result);
 		// }
-
-		function test_find_days_in_years()
-		{
-		//Arrange
-		$test_LeapYear = new LeapYear;
-		$input = '7/11/2014';
-
-		//Act
-		$result = $test_LeapYear->findLeapYear($input);
-
-		//Assert
-		$this->assertEquals('5113', $result);
-		}
+		//
+		// function test_find_days_in_years()
+		// {
+		// //Arrange
+		// $test_LeapYear = new LeapYear;
+		// $input = '7/11/2014';
+		//
+		// //Act
+		// $result = $test_LeapYear->findLeapYear($input);
+		//
+		// //Assert
+		// $this->assertEquals('5113', $result);
+		// }
+		//
+		// function test_find_days_in_month()
+		// {
+		// //Arrange
+		// $test_LeapYear = new LeapYear;
+		// $input = '7/11/2015';
+		//
+		// //Act
+		// $result = $test_LeapYear->findLeapYear($input);
+		//
+		// //Assert
+		// $this->assertEquals('5660', $result);
+		// }
 
 		function test_find_days_in_month()
 		{
 		//Arrange
 		$test_LeapYear = new LeapYear;
-		$input = '7/11/2015';
+		$input = '2016-2-18';
 
 		//Act
 		$result = $test_LeapYear->findLeapYear($input);
 
 		//Assert
-		$this->assertEquals('5660', $result);
+		$this->assertEquals('Thursday', $result);
 		}
 
 	}
